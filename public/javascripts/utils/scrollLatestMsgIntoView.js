6595bf9c97d99de4b00de05f8a6da50faa4280a4
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-05-27 16:12:39 +0200

    Remove console.log from scroll... util

--- public/javascripts/utils/scrollLatestMsgIntoView.js
+++ public/javascripts/utils/scrollLatestMsgIntoView.js
@@ -1,8 +1,6 @@
 function scrollLatestMsgIntoView() {
 	let allMsges = document.querySelectorAll('li');
-	console.log(allMsges);
 	let latestMsg = allMsges[allMsges.length - 1];
-	console.log(latestMsg);
 	latestMsg.scrollIntoView();
 }
 
