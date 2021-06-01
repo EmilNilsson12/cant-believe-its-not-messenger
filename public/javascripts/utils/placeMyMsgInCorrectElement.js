d0961afc30ed7014b61d8e1cf39941db5e1a3f78
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-06-01 14:56:43 +0200

    Refactor util files

--- public/javascripts/utils/placeMyMsgInCorrectElement.js
+++ public/javascripts/utils/placeMyMsgInCorrectElement.js
@@ -3,22 +3,17 @@ import { printMsgFromMeSameGroup } from '../modules/chatMsgs/printMsgFromMeSameG
 function placeMyMsgInCorrectElement(msg, chatLog) {
 	let divsInChatLog = document.querySelectorAll('ul > div');
 
+	let latestMsgGroup = divsInChatLog[divsInChatLog.length - 1];
+	let islatestMsgGroupOwner =
+		latestMsgGroup.dataset.sender == msg.user || false;
+
 	// If there exists previous msgs on screen
-	if (divsInChatLog.length) {
-		let latestMsgGroup = divsInChatLog[divsInChatLog.length - 1];
-		let islatestMsgGroupOwner = latestMsgGroup.dataset.sender == msg.user;
-
-		if (islatestMsgGroupOwner) {
-			// If latest msg was from me
-			printMsgFromMeSameGroup(msg, latestMsgGroup);
-		}
-
-		// If I am not owner of latest group
-		else {
-			printMsgFromMe(msg, chatLog);
-		}
+	// and I was the one who sent it
+	if (divsInChatLog.length && islatestMsgGroupOwner) {
+		printMsgFromMeSameGroup(msg, latestMsgGroup);
 	}
 	// If there DOES NOT exist previous msgs on screen
+	// or if the latest msg wasnt sent by me
 	else {
 		printMsgFromMe(msg, chatLog);
 	}
--- public/javascripts/utils/placeOtheUsersMsgInCorrectElement.js
+++ public/javascripts/utils/placeOtheUsersMsgInCorrectElement.js
@@ -3,21 +3,17 @@ import { printMsgFromOtherUser } from '../modules/chatMsgs/printMsgFromOtherUser
 function placeOtheUsersMsgInCorrectElement(msg, chatLog) {
 	let divsInChatLog = document.querySelectorAll('ul > div');
 
-	// If there exists previous msgs on screen
-	if (divsInChatLog.length) {
-		let latestMsgGroup = divsInChatLog[divsInChatLog.length - 1];
-		let islatestMsgGroupOwner = latestMsgGroup.dataset.sender == msg.user;
+	let latestMsgGroup = divsInChatLog[divsInChatLog.length - 1];
+	let islatestMsgGroupOwner =
+		latestMsgGroup.dataset.sender == msg.user || false;
 
-		if (islatestMsgGroupOwner) {
-			// If latest msg was from me
-			printMsgFromOtherUserSameGroup(msg, latestMsgGroup);
-		}
-		// If I am not owner of latest group
-		else {
-			printMsgFromOtherUser(msg, chatLog);
-		}
+	// If latest msg was from the same user
+	// add the new message inside the same group of msgs
+	if (divsInChatLog.length && islatestMsgGroupOwner) {
+		printMsgFromOtherUserSameGroup(msg, latestMsgGroup);
 	}
 	// If there DOES NOT exist previous msgs on screen
+	// or if the new msg has a different owner than the latest
 	else {
 		printMsgFromOtherUser(msg, chatLog);
 	}
