9bff46f87e726bdd80f6e4f6caefcbd12f0c054c
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-06-02 12:21:17 +0200

    Add strong tag to screenName on welcome msg

--- public/javascripts/modules/chatMsgs/printWelcomeMsgFromServer.js
+++ public/javascripts/modules/chatMsgs/printWelcomeMsgFromServer.js
@@ -2,7 +2,7 @@ function printWelcomeMsgFromServer(screenName, chatLog) {
 	chatLog.insertAdjacentHTML(
 		'beforeend',
 		`<div>
-			<li>Welcome! You are user: ${screenName}</li>
+			<li>Welcome! You are user: <strong>${screenName}</strong></li>
 		</div>`
 	);
 }
